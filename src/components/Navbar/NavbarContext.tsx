af04219c99ccec5412bef783db1ea71de64c2fb2
marcinhamiga <marcin.hamiga@proton.me>
2025-05-06 19:20:02 +0200

    Finished working on simple CRUD operations for speakers, speaker section of the app is partially complete (missing pagination), adjusted the style to be more coherent between all forms and form buttons

---
+++ src/components/Navbar/NavbarContext.tsx
@@ -0,0 +1,34 @@
+import {createContext, useContext, useEffect, useState} from "react";
+
+type NavbarContextType = {
+    isOpen: boolean;
+    toggle: () => void;
+};
+
+const NavbarContext = createContext<NavbarContextType>({
+    isOpen: true,
+    toggle: () => {}
+});
+
+export const useNavbar = () => useContext(NavbarContext);
+
+export const NavbarProvider = ({ children }: { children: React.ReactNode }) => {
+    const [isOpen, setIsOpen] = useState(false);
+
+    useEffect(() => {
+        const saved = localStorage.getItem("navbarOpen") === "true";
+        setIsOpen(saved);
+    }, []);
+
+    useEffect(() => {
+        localStorage.setItem("navbarOpen", String(isOpen));
+    }, [isOpen]);
+
+    const toggle = () => setIsOpen((prev) => !prev);
+
+    return (
+        <NavbarContext.Provider value={{ isOpen, toggle}}>
+            {children}
+        </NavbarContext.Provider>
+    )
+}
--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,14 @@ import { createRoot } from 'react-dom/client'
 import './App.css'
 import App from './App.tsx'
 import {BrowserRouter} from "react-router-dom";
+import {NavbarProvider} from "./components/Navbar/NavbarContext.tsx";
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
       <BrowserRouter>
-          <App />
+          <NavbarProvider>
+              <App />
+          </NavbarProvider>
       </BrowserRouter>
   </StrictMode>,
 )
